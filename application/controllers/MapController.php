f226f5f6e3caea12f2086604ce7f10f7905d44c8
sumit <rounnn8@gmail.com>
2018-03-30 12:32:17 +0545

    new files added for map

---
+++ application/controllers/MapController.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MapController extends CI_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+
+    $this->load->helper('url');
+    $this->load->model('Map_model');
+  }
+
+
+public function test(){
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+}//end
---
+++ application/models/Map_model.php
@@ -0,0 +1,16 @@
+<?php
+class Map_model extends CI_Model {
+
+
+
+public function test(){
+
+
+
+
+
+}
+
+
+
+}
