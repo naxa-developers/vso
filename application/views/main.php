4230f2dc55e04dfdd67bff08b68a6e4b6e4d208a
sumit <rounnn8@gmail.com>
2018-12-21 13:11:12 +0545

    landslide link

--- application/views/main.php
+++ application/views/main.php
@@ -41,11 +41,13 @@
                 </div>
               </div>
               <div class="col-md-2">
+                <a href="category?tbl=land_slides&&name=Land Slides">
                 <div class="disaster-summary-item" data-mh="summary-item">
                     <img src="<?php echo base_url()?>assets/icon/landslide1.png" height="64">
-                  <h6><?php echo LANDSLIDE ?></h6>
-                  <h4>  106<?php //echo !empty($report_inci['Landslide'])?$report_inci['Landslide']:'0'?></h4>
+                  <h6 style="color:black;"><?php echo LANDSLIDE ?></h6>
+                  <h4 style="color:black;">  106<?php //echo !empty($report_inci['Landslide'])?$report_inci['Landslide']:'0'?></h4>
                 </div>
+                </a>
               </div>
               <div class="col-md-2">
                 <div class="disaster-summary-item" data-mh="summary-item">
